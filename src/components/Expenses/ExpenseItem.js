824823b3835abe1658ba957d1ddb7c3337bd7486
Harrison Tran-Chung <tranchung@live.com>
2021-10-19 20:30:24 -0500

    2nd commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import React from "react";
+
+const ExpenseItem = (props) => {
+    
+    const clickHandler = () => {
+        console.log('Clicked');
+    };
+
+
+    return(
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            <button onClick={clickHandler}> Change Title</button>
+        </Card>
+    );
+}
+
+export default ExpenseItem;
